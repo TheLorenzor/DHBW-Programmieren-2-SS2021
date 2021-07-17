79fea1bdb1706897861e4543b03cdaddb9065b24
the_loraxxz <the_loraxxz@web.de>
2021-07-17 10:54:22 +0200

    Test

--- src/main.java
+++ src/main.java
@@ -1,6 +1,20 @@
 import frontend.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
 public class main {
     public static void main(String []args){
-        GUI frame = new GUI();
+        JFrame t = new JFrame();
+        JPanel auftrag = new JPanel();
+        ImageIcon back = new ImageIcon(Objects.requireNonNull(main.class.getClassLoader().getResource("Background2.png")));
+        JLabel backgdrop = new JLabel(back);
+        t.setDefaultCloseOperation(3);
+        t.setPreferredSize(new Dimension(100,100));
+        t.pack();
+        t.setContentPane(backgdrop);
+        t.setVisible(true);
     }
 }
+
